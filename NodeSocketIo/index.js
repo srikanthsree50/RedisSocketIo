b86dc45bddca1f3343a7dcee9cd832a6f2c239eb
srikanthsree50 <srikanthsree50@gmail.com>
2021-06-17 20:40:22 +0530

    node socket commit

---
+++ NodeSocketIo/index.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+socketio = require('socket.io')
+const path = require('path');
+var app = express();
+var server = app.listen(8080);
+var io = socketio(server);
+
+app.use(express.static('static'));
+
+io.on('connection',(socket) => {
+    console.log('A socket is now open...');
+console.log(socket);
+
+});
